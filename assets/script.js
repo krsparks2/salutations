e89aaa523cc06e5f6c00cedddac67c72bb438d90
LeaBryant <ldbryant3220@gmail.com>
2022-06-11 13:01:16 -0700

    im back

--- assets/script.js
+++ assets/script.js
@@ -55,8 +55,6 @@ function sunEvent(results) {
     var sunriseEl = document.createElement("h2");
     var sunsetEl = document.createElement("h2");
     var highnoonEl = document.createElement("h2");
-
-    //sunriseEL.innerText = sunrise;
     
     sunriseEl.textContent = "sunrise: " + sunrise;
     sunsetEl.textContent = "sunset: " + sunset;
