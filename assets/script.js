f90590812ec3a7bb20fed9317721ce5fb10d3617
tracy rose guajardo <tracyroseguajardo@gmial.com>
2022-06-15 19:14:33 -0700

    added local storage

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,7 @@ var eveningEl = document.getElementById("evening");
 //Handles user location input
 function locationInput() {
     var location = locationEl.value;
+    localStorage.setItem("location", location);
 
     getCoordinates(location);
 }
@@ -37,6 +38,7 @@ function getCoordinates(location) {
 //Passes called coordinates through API to grab sun event times
 function makeCall(lat, lon) {
     var date = dateEl.value;
+    localStorage.setItem("date", date);
 
     fetch("https://api.sunrise-sunset.org/json?lat=" + lat + "&lng=" + lon + "&date=" + date + "&formatted=0").then(function (result) {
         return result.json()
@@ -56,12 +58,17 @@ function sunEvent(results) {
     var sunrise = new Date(results.sunrise).toLocaleTimeString();
     var highnoon = new Date(results.solar_noon).toLocaleTimeString();
     var sunset = new Date(results.sunset).toLocaleTimeString();
+
     // var sunrise = results.sunrise;
     // var sunset = results.sunset;
     // var highnoon = results.solar_noon;
     // console.log(sunrise);
     // console.log(highnoon);
     // console.log(sunset);
+
+    localStorage.setItem("sunrise", sunrise);
+    localStorage.setItem("highnoon", highnoon);
+    localStorage.setItem("sunset", sunset);
 
     var sunriseEl = document.createElement("h2");
     var highnoonEl = document.createElement("h2");
